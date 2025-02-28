f760046cd05d1d4ec05f4304497b2c5e4093bee1
a8ly <adnreylysov@gmail.com>
2025-02-28 09:33:39 +0300

    init

--- routes/web.php
+++ routes/web.php
@@ -1,55 +1,15 @@
 <?php
 
-use App\Http\Controllers\DBItemController;
-use App\Http\Controllers\PriceIceController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\SpawnProductsCostController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+    return Inertia::render('Welcome');
+})->name('home');
 
-Route::get('/dashboard', function () {
+Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::middleware('auth')->group(function () {
-
-    Route::prefix('minecraft-db')->name('minecraft-db.')->controller(DBItemController::class)->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::post('/', 'store')->name('store');
-        Route::patch('/{DBItem}', 'update')->name('update');
-        Route::delete('/{DBItem}', 'destroy')->name('destroy');
-    });
-
-    Route::prefix('products-cost')->name('products-cost.')->controller(SpawnProductsCostController::class)->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::post('/', 'store')->name('store');
-        Route::patch('/{ProductCost}', 'update')->name('update');
-        Route::delete('/{ProductCost}', 'destroy')->name('destroy');
-    });
-
-    Route::prefix('price-ice')->name('price-ice.')->controller(PriceIceController::class)->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::post('/', 'store')->name('store');
-        Route::patch('/{PriceIce}', 'update')->name('update');
-        Route::delete('/{PriceIce}', 'destroy')->name('destroy');
-    });
-
-});
-
+require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
